3ecf983192cf66c413d754eac75829eeb415e24c
Amanda Bressam <amandabressam@gmail.com>
2022-04-25 13:50:59 -0300

    Create service to use retrofit

---
+++ app/src/main/java/com/example/currencyconverter/remote/CurrencyConverterService.kt
@@ -0,0 +1,15 @@
+package com.example.currencyconverter.remote
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class CurrencyConverterService {
+    companion object {
+        fun getRetrofitInstance(path: String) : Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(path)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
